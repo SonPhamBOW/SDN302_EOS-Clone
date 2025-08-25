37d619871cc2e4ba38415827910e60483e517c0d
nhsonn1 <hoangsonnguyen610@gmail.com>
2025-08-25 20:02:31 +0700

    fix imports in student controller

--- server/controller/student.controller.js
+++ server/controller/student.controller.js
@@ -1,5 +1,5 @@
-import { User } from "../models/user.model.js";
-import { CourseStudent } from "../models/course-student.model.js";
+import { User } from "../models/User.js";
+import { CourseStudent } from "../models/CourseStudent.js";
 /**
  * Middleware inside controller file
  */
