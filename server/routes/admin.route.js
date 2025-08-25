6b4b876338ea7a1cd0bb1cdc7cdb2e2d58d08be8
Nguyen Thanh Phuc <phucnthe170081@gmail.com>
2025-08-26 03:16:06 +0700

    update Results Management with role adminadmin

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import * as adminController from "../controller/admin.controller.js";
 import { adminRoute } from "../middleware/auth.middleware.js";
+
 const adminRouter = Router();
+
+adminRouter.get("/exam-results/export", adminRoute, adminController.exportExamResultsExcel);
 
 adminRouter.get("/students", adminRoute, adminController.getAllStudent);
 
